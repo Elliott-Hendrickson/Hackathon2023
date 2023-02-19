13ea645fb7fbab74e1c37907b062b5eac43bea65
Elliott-Hendrickson <100046451+Elliott-Hendrickson@users.noreply.github.com>
2023-02-19 01:57:10 -0800

    Update BrowserCollector.cs

--- DuckAPP/DuckAPP/BrowserCollector/BrowserCollector.cs
+++ DuckAPP/DuckAPP/BrowserCollector/BrowserCollector.cs
@@ -43,7 +43,34 @@ namespace DuckAPP
                     }
                 }
             }
-            return "ding!";
+            var rand = new Random();
+            int number = rand.Next() % 10;
+            switch (number)
+            {
+                case 0:
+                    return "Im proud of you!";
+                case 1:
+                    return "When I was your age I was sponsored by dawn";
+                case 2:
+                    return "Only a little bit further!";
+                case 3:
+                    return "Did I ever tell you about the time I was in a hackathon?";
+                case 4:
+                    return "You could be doing better";
+                case 5:
+                    return "0_0";
+                case 6:
+                    return "You call yourself a stem major?";
+                case 7:
+                    return "The MLC is always open to help";
+                case 8:
+                    return "Go to Owen library";
+                case 9:
+                    return "AAAAHHHHHH";
+                case 10:
+                    return "We are working together";
+            }
+            return "HEEHEEHAAHAA Literally Insane";
         }
         private static string warantedSite (string siteName)
         {
