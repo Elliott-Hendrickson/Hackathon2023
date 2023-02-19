d7ef320a9de14d460604c77a76347472811aaa61
cooltot1 <53503640+cooltot1@users.noreply.github.com>
2023-02-18 21:00:18 -0800

    .gitignore cleared + Context menu added

--- DuckAPP/DuckAPP/Form1.Designer.cs
+++ DuckAPP/DuckAPP/Form1.Designer.cs
@@ -31,26 +31,59 @@
         {
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.funToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.ContextMenuStrip = this.contextMenuStrip1;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
             this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = global::DuckAPP.Properties.Resources.Penguin;
-            this.button1.Location = new System.Drawing.Point(360, 26);
+            this.button1.Location = new System.Drawing.Point(360, 27);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(386, 505);
             this.button1.TabIndex = 0;
             this.button1.Text = "\r\n";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem,
+            this.funToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 70);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitEvent);
+            // 
+            // funToolStripMenuItem
+            // 
+            this.funToolStripMenuItem.Name = "funToolStripMenuItem";
+            this.funToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.funToolStripMenuItem.Text = "Fun";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
             // 
             // button2
             // 
@@ -82,6 +115,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -92,5 +126,9 @@
         private TextBox textBox1;
         private Button button2;
         private Button button3;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem quitToolStripMenuItem;
+        private ToolStripMenuItem funToolStripMenuItem;
+        private ToolStripMenuItem settingsToolStripMenuItem;
     }
 }
